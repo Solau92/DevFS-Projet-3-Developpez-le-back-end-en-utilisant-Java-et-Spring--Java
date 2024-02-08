f1b45808352c137c8d00cee99b8f83ccb75ca659
Sophie LAURENT <sophielaurent92@gmail.com>
2024-02-08 14:46:34 +0100

    first commit

---
+++ src/test/java/com/ChaTop/Rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChaTop.Rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
